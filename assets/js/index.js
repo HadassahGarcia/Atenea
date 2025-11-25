1323b919f8674b28aca73c49ef2d988a58bcd12b
Hadassah Garcia <hadassahalejandra123@gmail.com>
2025-11-25 14:54:39 -0600

    Creacion del index y modales en su js

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// Abrir modal según botón
+document.querySelectorAll(".sidebar button").forEach(btn => {
+  btn.addEventListener("click", e => {
+    const modalID = btn.dataset.modal;
+    document.getElementById(modalID).classList.add("active");
+  });
+});
+
+// Cerrar modal
+document.querySelectorAll(".modal .close").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.closest(".modal").classList.remove("active");
+  });
+});
+
+// Cerrar al hacer clic fuera
+document.querySelectorAll(".modal").forEach(modal => {
+  modal.addEventListener("click", e => {
+    if (e.target === modal) modal.classList.remove("active");
+  });
+});
